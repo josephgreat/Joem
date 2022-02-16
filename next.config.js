02efc16890d56c6d7255eea0a777900d72461292
Joseph <66772747+josephgreat@users.noreply.github.com>
2022-02-16 15:52:35 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['bayut.com', 'bayut-production.s3.eu-central-1.amazonaws.com'],
+    // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],    
+    // imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  }
+}
